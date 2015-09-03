7c34b3975120a309ac19c4770bd2c33ff284417e
unknown <raykim@kimwoonyoung.office.afreecatv.com>
2015-09-03 16:49:23 +0900

    no message

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ io.sockets.on('connection', function (socket) {
         sub.publish("emrchat",msg.message);  
       }
       else if(msg.type == "setUsername"){
-        sub.publish("emrchat", "A New User is connected : " + msg.user);
+        sub.publish("emrchat", {username:msg.user, msg:"A New User is connected : " + msg.user});
         client.sadd("onlineUsers",msg.user);
       }
     });
